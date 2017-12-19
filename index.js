bacf29e4fa6d4ece3131697bb8f9beb7ee823f08
cnbs5 <879700945@qq.com>
2017-12-19 15:25:54 +0800

    launch js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import setup from './js/setup';
 
-AppRegistry.registerComponent('MoodCare', () => App);
+AppRegistry.registerComponent('MoodCare', () => setup);
