510432dd64cdac01e15f7f37c5f103f4eabc30e3
cnbs5 <879700945@qq.com>
2017-12-19 17:31:55 +0800

    唔 ...

--- js/common/LaunchScreen.js
+++ js/common/LaunchScreen.js
@@ -4,19 +4,50 @@ import React from "react";
 import F8Colors from "./F8Colors";
 import {Dimensions, StyleSheet, View, Image} from "react-native";
 
+import LoginScreen from "../login/LoginScreen"
+
 const WIN_WIDTH = Dimensions.get("window").width, WIN_HEIGHT = Dimensions.get("window").height;
 
 
 class LaunchScreen extends React.Component {
-    render() {
-        return (
-            <View style={[styles.container, this.props.style]}>
-                <Image
-                    source={require("./img/launchscreen.png")}
-                    style={styles.image}
-                />
-            </View>
+
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+            loggedIn: false
+        };
+    }
+
+    componentDidMount() {
+        this.timer = setTimeout(
+            () => {
+                this.setState({
+                    isLoading: false,
+                });
+            },
+            1000
         );
+    }
+
+    componentWillUnmount() {
+        this.timer && clearTimeout(this.timer);
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={[styles.container, this.props.style]}>
+                    <Image
+                        source={require("./img/launchscreen.png")}
+                        style={styles.image}
+                    />
+                </View>
+            )
+        } else {
+            return <LoginScreen/>
+        }
+
     }
 }
 
